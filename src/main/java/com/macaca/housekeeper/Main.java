ebf36bab44cac18d67fa3473110061b1de8abdc7
Mayowa Aj <mayowaaj69@hotmail.co.uk>
2018-07-22 18:20:32 +0100

    commit

--- src/main/java/com/macaca/housekeeper/Main.java
+++ src/main/java/com/macaca/housekeeper/Main.java
@@ -1,7 +1,12 @@
 package com.macaca.housekeeper;
 
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
 public class Main {
     public static void main(String[] args) {
-
+        String token = "NDcwNTc0OTcyMDU4OTkyNjUw.DjYs3g.bXWKBy0aw5tqo57_sYpEmnSJe0g";
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+        System.out.println("Logged in!");
     }
 }
