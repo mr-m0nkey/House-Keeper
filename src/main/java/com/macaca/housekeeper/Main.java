70d4a5dc7de1039d4ebd4de3ea12a6817cb82c43
Mayowa Aj <mayowaaj69@hotmail.co.uk>
2018-07-24 00:07:26 +0100

    Created a .env

--- src/main/java/com/macaca/housekeeper/Main.java
+++ src/main/java/com/macaca/housekeeper/Main.java
@@ -1,12 +1,24 @@
 package com.macaca.housekeeper;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
 public class Main {
     public static void main(String[] args) {
-        String token = "NDcwNTc0OTcyMDU4OTkyNjUw.DjYs3g.bXWKBy0aw5tqo57_sYpEmnSJe0g";
+        Dotenv dotenv = Dotenv.configure().directory("./").load();
+
+        String token = dotenv.get("TOKEN");
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
         System.out.println("Logged in!");
+
+        api.addMessageCreateListener(event -> {
+            if (event.getMessage().getContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
     }
 }
