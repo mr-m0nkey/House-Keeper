283cd7fbd14a70d1127b5cc733b189acdb794ca4
Mayowa Aj <mayowaaj69@hotmail.co.uk>
2018-11-07 22:19:25 +0100

    commit

--- src/main/java/com/macaca/housekeeper/Commands/GetStatus.java
+++ src/main/java/com/macaca/housekeeper/Commands/GetStatus.java
@@ -7,12 +7,18 @@ import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
 import java.awt.*;
+import java.util.Date;
 
 public class GetStatus implements MessageCreateListener {
     @Override
     public void onMessageCreate(MessageCreateEvent messageCreateEvent) {
         String message = messageCreateEvent.getMessage().getContent();
         String owner = messageCreateEvent.getApi().getOwner().join().getName();
+        long seconds = (new Date().getTime() - Main.startupTime) / 1000;
+        long minutes = seconds / 60;
+        long hours = minutes / 60;
+        long days = hours / 24;
+        String time = days + " days, " + hours % 24 + " hours, " + minutes % 60 + " minutes and " + seconds % 60 + " seconds";
         if(message.equalsIgnoreCase("!hk status")){
             if(messageCreateEvent.getMessage().getAuthor().isBotOwner()){
                 new MessageBuilder()
@@ -21,6 +27,8 @@ public class GetStatus implements MessageCreateListener {
                                 .setDescription("Bot Details")
                                 .addField("Owner", owner)
                                 .addField("Server count", "" + Main.api.getServers().size())
+                                .addField("Uptime", time)
+                                .addField("Invite link", Main.api.createBotInvite())
                                 .setColor(Color.RED))
                         .send(messageCreateEvent.getChannel());
             }
--- src/main/java/com/macaca/housekeeper/Main.java
+++ src/main/java/com/macaca/housekeeper/Main.java
@@ -5,11 +5,17 @@ import com.macaca.housekeeper.Utils.CommandUtils;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.activity.ActivityType;
 import org.javacord.api.entity.message.MessageSet;
+
+import java.io.File;
+import java.util.Date;
 
 public class Main {
 
     public static  DiscordApi api;
+
+    public static long startupTime;
 
     public static void main(String[] args) {
 
@@ -20,22 +26,29 @@ public class Main {
         //logs in
         api = new DiscordApiBuilder().setToken(token).login().join();
         System.out.println("Logged in!");
+        startupTime = new Date().getTime();
 
-        setupCommands();
+
+        setupBot();
         setupListeners();
 
 
-    }
 
-    private static void setupCommands(){
-        api.addMessageCreateListener(new GetStatus());
     }
 
     private static void setupListeners(){
+        api.addMessageCreateListener(new GetStatus());
         api.addServerMemberLeaveListener(serverMemberLeaveEvent -> {
             // TODO: 01/08/2018 set main channel
             serverMemberLeaveEvent.getServer().getChannelsByNameIgnoreCase("general").get(0).asServerTextChannel().get()
                     .sendMessage("Sayounara " + serverMemberLeaveEvent.getUser().getName());
         });
+    }
+
+    private static void setupBot(){
+        api.updateActivity(ActivityType.WATCHING, "paint dry");
+        if (api.getYourself().hasDefaultAvatar()){
+            api.updateAvatar(new File("files/profile.jpeg"));
+        }
     }
 }
